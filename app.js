b49fe5dfccec59d24e8036edf82dbe3ed11727a6
nateos <sodjanathan@gmail.com>
2022-12-04 23:42:07 +0000

    chore: import router and controller modules

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// routers
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -14,18 +18,17 @@ const connectDB = require("./db/connect");
 
 const authenticateUser = require("./middleware/authentication");
 
-// routers
-const authRouter = require("./routes/auth");
-const jobsRouter = require("./routes/jobs");
-
 // extra packages
 
 // routes
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
+
 app.get("/", (req, res) => {
   res.send("finally works");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // start server
 const start = async () => {
