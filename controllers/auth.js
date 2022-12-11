5669815d95aecb427c852c70acda1e45e710439d
nateos <sodjanathan@gmail.com>
2022-12-11 22:32:53 +0000

    feat: hash user password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,17 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   // if (!email || !password || !name) {  //* redundant validation because of mongoose validation, still useful on other controllers
   //   throw new BadRequestError("Please supply email, password and name");
   // }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = await User.create({ ...req.body });
+  const user = await User.create({ name, email, password: hashedPassword });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
