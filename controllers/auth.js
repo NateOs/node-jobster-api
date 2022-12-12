9014e1344ee7d76913b633b70a4acb5e60369b31
nateos <sodjanathan@gmail.com>
2022-12-12 00:11:43 +0000

    feat: pre save middleware for hashing pass

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,10 +8,9 @@ const register = async (req, res) => {
   // if (!email || !password || !name) {  //* redundant validation because of mongoose validation, still useful on other controllers
   //   throw new BadRequestError("Please supply email, password and name");
   // }
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
 
-  const user = await User.create({ name, email, password: hashedPassword });
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,12 @@ const UserSchema = new mongoose.Schema({
     required: true,
     minlength: 8,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  // next(); use either async/await or call next()
 });
 
 module.exports = mongoose.model("User", UserSchema);
