bc0cd5911a13255c569160a755856de20c24f8f0
nateos <sodjanathan@gmail.com>
2022-12-15 14:09:31 +0000

    feat: /register logic, create JWT, instance method

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -9,9 +10,10 @@ const register = async (req, res) => {
   //   throw new BadRequestError("Please supply email, password and name");
   // }
 
-
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,5 +30,16 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
   // next(); use either async/await or call next()
 });
+
+// this is an instance method, allows you to abstract logic, and keep controller clean
+UserSchema.methods.createJWT = () => {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    },
+  );
+};
 
 module.exports = mongoose.model("User", UserSchema);
