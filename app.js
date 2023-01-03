7a9bd4da263e3a6607e5d60c7252e341915989e3
nateos <sodjanathan@gmail.com>
2023-01-03 00:50:57 +0000

    feat: swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors"); // allows requests across domains
 const xss = require("xss-clean"); // sanitizes user POST, GET queries
 const rateLimit = require("express-rate-limit"); // prevents incessant spam requests from client
 
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -43,11 +48,11 @@ const authenticateUser = require("./middleware/authentication");
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
+// reach the api docs
 app.get("/", (req, res) => {
-  res.send(
-    "This is the simple job API, login first and proceed to make your API request.",
-  );
+  res.send('<h1>API Documentation</h1><a href="/api-docs">Documentation</a>');
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 
